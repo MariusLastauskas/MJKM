65691737c1cab559534cbbd63fa83f1ab5312adc
MariusLastauskas <marius20122@gmail.com>
2016-05-02 23:27:53 +0200

    pridėtas highand

--- evento-free-music-event-template/pokeris/pokeris_script.js
+++ evento-free-music-event-template/pokeris/pokeris_script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 	var cardDeck;
+	p1=[];
+	p2=[];
+	p3=[];
+	p4=[];
+	// var p1strenght;
+	// var p2strenght;
+	// var p3strenght;
+	// var p4strenght;
+
 	shufleCards();
 	function shufleCards(){
 		resetDeck();
@@ -10,6 +19,19 @@ $(document).ready(function(){
 		}
 		for(var i=0;i<5;i++){
 			dealTable();}
+		p1.unshift(0);
+		p2.unshift(0);
+		p3.unshift(0);
+		p4.unshift(0);
+		findHighCard(p1);
+		findHighCard(p2);
+		findHighCard(p3);
+		findHighCard(p4);
+		
+		console.log(p1[0]);
+		console.log(p2[0]);
+		console.log(p3[0]);
+		console.log(p4[0]);
 	}
 
 	function resetDeck(){
@@ -19,12 +41,23 @@ $(document).ready(function(){
 	function dealHand(player){
 		var drawnCard;
 		drawnCard=Math.floor((Math.random()*cardDeck.length)+1);
-		console.log(drawnCard);
 		if(cardDeck[drawnCard-1]!=0){
 			$("#"+player+"player").html($("#"+player+"player").html()+" "+cardDeck[drawnCard-1]);
+			//console.log(player);
+			if(player==1){
+				p1.unshift(drawnCard-1);
+				}
+				else if(player==2){
+					p2.unshift(drawnCard-1);
+				}
+				else if(player==3){
+					p3.unshift(drawnCard-1);
+				}
+				else if(player==4){
+					p4.unshift(drawnCard-1);
+				}
 			cardDeck[drawnCard-1]=0;}
-				else{dealHand(player);
-					console.log('DDDD')}
+				else{dealHand(player);}
 	}
 
 	function dealTable(){
@@ -32,9 +65,69 @@ $(document).ready(function(){
 		drawnCard=Math.floor((Math.random()*cardDeck.length)+1);
 		if(cardDeck[drawnCard-1]!=0){
 			$("#table").html($("#table").html()+" "+cardDeck[drawnCard-1]);
+			p1.unshift(drawnCard-1);
+			p2.unshift(drawnCard-1);
+			p3.unshift(drawnCard-1);
+			p4.unshift(drawnCard-1);
 		cardDeck[drawnCard-1]=0;
 		}
 			else{dealTable();}
+	}
+
+	function findHighCard(player){
+		for(var i=0;i<=7;i++){
+			if(player[i]==1||player[i]==14||player[i]==27||player[i]==40){
+				player[0]=1;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==2||player[i]==15||player[i]==28||player[i]==41){
+				player[0]=2;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==3||player[i]==16||player[i]==29||player[i]==42){
+				player[0]=3;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==4||player[i]==17||player[i]==30||player[i]==43){
+				player[0]=4;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==5||player[i]==18||player[i]==31||player[i]==44){
+				player[0]=5;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==6||player[i]==19||player[i]==32||player[i]==45){
+				player[0]=6;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==7||player[i]==20||player[i]==33||player[i]==46){
+				player[0]=7;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==8||player[i]==21||player[i]==34||player[i]==47){
+				player[0]=8;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==9||player[i]==22||player[i]==35||player[i]==48){
+				player[0]=9;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==10||player[i]==23||player[i]==36||player[i]==49){
+				player[0]=10;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==11||player[i]==24||player[i]==37||player[i]==50){
+				player[0]=11;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==12||player[i]==25||player[i]==38||player[i]==51){
+				player[0]=12;
+			}}
+		for(var i=0;i<=7;i++){
+			if(player[i]==0||player[i]==13||player[i]==26||player[i]==39){
+				player[0]=13;
+			}}
+			//console.log(player)
 	}
 
 })
