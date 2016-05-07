efd11d99a79388f12bbb708813a38de338d0e1f1
MariusLastauskas <marius20122@gmail.com>
2016-05-07 12:19:57 +0200

    randama ranka

--- evento-free-music-event-template/pokeris/pokeris_script.js
+++ evento-free-music-event-template/pokeris/pokeris_script.js
@@ -23,15 +23,24 @@ $(document).ready(function(){
 		p2.unshift(0);
 		p3.unshift(0);
 		p4.unshift(0);
-		findHighCard(p1);
-		findHighCard(p2);
-		findHighCard(p3);
-		findHighCard(p4);
+		// findHighCard(p1);
+		// findHighCard(p2);
+		// findHighCard(p3);
+		// findHighCard(p4);
+		var p1hand=[0];
+		var p2hand=[0];
+		var p3hand=[0];
+		var p4hand=[0];
 
-		findHighestPair(p1);
-		findHighestPair(p2);
-		findHighestPair(p3);
-		findHighestPair(p4);
+		findHighestPair(p1,p1hand);
+		findHighestPair(p2,p2hand);
+		findHighestPair(p3,p3hand);
+		findHighestPair(p4,p4hand);
+
+		console.log("player1Hand");
+		for(var i=0;i<p1hand.length;i++){
+			console.log(p1hand[i])
+		}
 		
 		// console.log(p1[0]);
 		// console.log(p2[0]);
@@ -79,7 +88,7 @@ $(document).ready(function(){
 			else{dealTable();}
 	}
 
-	function findHighCard(player){
+	function findHighCard(player,hand){
 		for(var i=0;i<=7;i++){
 			if(player[i]==1||player[i]==14||player[i]==27||player[i]==40){
 				player[0]=1;
@@ -135,20 +144,20 @@ $(document).ready(function(){
 			//console.log(player)
 	}
 
-	function findHighestPair(player){
-		var numberOfa=0;
-		var numberOf2=0;
-		var numberOf3=0;
-		var numberOf4=0;
-		var numberOf5=0;
-		var numberOf6=0;
-		var numberOf7=0;
-		var numberOf8=0;
-		var numberOf9=0;
-		var numberOf10=0;
-		var numberOfj=0;
-		var numberOfq=0;
-		var numberOfk=0;
+	function findHighestPair(player,hand){
+		// var numberOfa=0;
+		// var numberOf2=0;
+		// var numberOf3=0;
+		// var numberOf4=0;
+		// var numberOf5=0;
+		// var numberOf6=0;
+		// var numberOf7=0;
+		// var numberOf8=0;
+		// var numberOf9=0;
+		// var numberOf10=0;
+		// var numberOfj=0;
+		// var numberOfq=0;
+		// var numberOfk=0;
 		var numberOfCards=[0,0,0,0,0,0,0,0,0,0,0,0,0];
 		for(var i=1;i<=player.length;i++){
 			if(player[i]==0||player[i]==13||player[i]==26||player[i]==39){
@@ -221,20 +230,187 @@ $(document).ready(function(){
 		//console.log(cardsx1[0])
 		if(cardsx4.length>0){
 			console.log("4x "+cardsx4[0]+" 1x "+cardsx1[0]);
+			hand[0]=8;
+			hand.push(cardsx4[0]);
+			hand.push(cardsx1[0]);
+			console.log("FOUR OF A KIND")
 		}
 		else if(cardsx3.length>0){
 			if(cardsx2.length>0){
 				console.log("3x "+cardsx3[0]+" 2x "+cardsx2[0])
+				hand[0]=7;
+				hand.push(cardsx3[0]);
+				hand.push(cardsx2[0]);
+				console.log("FULL HOUSE")
 			}
-			else{console.log("3x "+cardsx3[0]+" 1x "+cardsx1[0]+" "+cardsx1[1])}
+			else{console.log("3x "+cardsx3[0]+" 1x "+cardsx1[0]+" "+cardsx1[1])
+				hand[0]=4;
+				hand.push(cardsx3[0]);
+				hand.push(cardsx1[0]);
+				hand.push(cardsx1[1]);
+				console.log("THREE OF A KIND")
+			}
 		}
 		else if(cardsx2.length>0){
 			if(cardsx2.length>1){
 				console.log("2x "+cardsx2[0]+" "+cardsx2[1]+" 1x "+cardsx1[0]);
+				hand[0]=3;
+				hand.push(cardsx2[0]);
+				hand.push(cardsx2[1]);
+				hand.push(cardsx1[0]);
+				console.log("TWO PAIRS")
 			}
-			else{console.log("2x "+cardsx2[0]+" 1x "+cardsx1[0]+" "+cardsx1[1]+" "+cardsx1[2])}
+			else{console.log("2x "+cardsx2[0]+" 1x "+cardsx1[0]+" "+cardsx1[1]+" "+cardsx1[2])
+				hand[0]=2;
+				hand.push(cardsx2[0]);
+				hand.push(cardsx1[0]);
+				hand.push(cardsx1[1]);
+				hand.push(cardsx1[2]);
+				console.log("ONE PAIR")
+			}
 		}
-		else{console.log("1x "+cardsx1[0]+" "+cardsx1[1]+" "+cardsx1[2]+" "+cardsx1[3]+" "+cardsx1[4])}
-	}
+		else{console.log("1x "+cardsx1[0]+" "+cardsx1[1]+" "+cardsx1[2]+" "+cardsx1[3]+" "+cardsx1[4])
+			hand[0]=1;
+			hand.push(cardsx1[0]);
+			hand.push(cardsx1[1]);
+			hand.push(cardsx1[2]);
+			hand.push(cardsx1[3]);
+			hand.push(cardsx1[4]);
+			console.log("HIGH HAND");
+		}
 
+		for(var i=0;i<9;i++){
+			if(numberOfCards[i]>0&&numberOfCards[i+1]>0&&numberOfCards[i+2]>0&&numberOfCards[i+3]>0&&numberOfCards[i+4]>0){   //finding straight
+				console.log("STRAIGHT"+(i+2));
+				if(hand[0]<=5){
+					hand.unshift(i+2);
+					hand.unshift(5);
+				}
+			}
+
+		}
+		if(numberOfCards[0]>0&&numberOfCards[1]>0&&numberOfCards[2]>0&&numberOfCards[3]>0&&numberOfCards[12]>0){
+				console.log("STRAIGHT")
+				if(hand[0]<=5){
+					hand.unshift(0);
+					hand.unshift(5);
+				}
+			}
+
+			var spades=0;
+			var clubs=0;
+			var diamonds=0;
+			var hearths=0;
+		for(var i=1;i<=player.length;i++){   //finding flush
+			if(player[i]<13){
+				spades++;
+			}
+			else if(player[i]>12&&player[i]<26){
+				clubs++;
+			}
+			else if(player[i]>25&&player[i]<39){
+				hearths++;
+			}
+			else if(player[i]>38){
+				diamonds++;
+			}
+		}
+			console.log("spades="+spades+" clubs="+clubs+" diamonds="+diamonds+" hearths="+hearths)	
+		if(spades>4){
+			var highspades=[];
+			for(var i=0;i<player.length;i++){
+				if(player[i]<13){
+					highspades.push(player[i]);
+				}
+			}
+			console.log("FLUSH");
+			highspades.sort(function(a, b){return a-b});
+			if(highspades[0]==1){
+				highspades.push(14);
+			}
+			highspades.sort(function(a, b){return a-b});
+			if(hand[0]<5){
+				for(var i=0;i<highspades.length;i++){
+					console.log(highspades[i]+1)
+					hand.unshift(highspades[i]+1);
+				}
+				hand.unshift(6);}
+				else if(hand[0]==5){
+					console.log('STRAIGHT FLUSH')
+					hand[0]=9;
+				}
+		}
+		if(clubs>4){
+			var highclub=[];
+			for(var i=0;i<player.length;i++){
+				if(player[i]>12&&player[i]<26){
+					highclub.push(player[i]-12);
+				}
+			}
+			console.log("FLUSH");
+			highclub.sort(function(a, b){return a-b});
+			if(highclub[0]==1){
+				highclub.push(14);
+			}
+			highclub.sort(function(a, b){return a-b});
+			if(hand[0]<5){
+				for(var i=0;i<highclub.length;i++){
+					console.log(highclub[i]+1)
+					hand.unshift(highclub[i]+1);
+				}
+				hand.unshift(6);}
+				else if(hand[0]==5){
+					console.log('STRAIGHT FLUSH')
+					hand[0]=9;
+				}
+		}
+		if(diamonds>4){
+			var highdiamonds=[];
+			for(var i=0;i<player.length;i++){
+				if(player[i]>38){
+					highdiamonds.push(player[i]-38);
+				}
+			}
+			highdiamonds.sort(function(a, b){return a-b});
+			console.log("FLUSH");
+			if(highdiamonds[0]==1){
+				highdiamonds.push(14);
+			}
+			highdiamonds.sort(function(a, b){return a-b});
+			if(hand[0]<5){
+				for(var i=0;i<highdiamonds.length;i++){
+					console.log(highdiamonds[i]+1)
+					hand.unshift(highdiamonds[i]+1);
+				}
+				hand.unshift(6);}
+				else if(hand[0]==5){
+					console.log('STRAIGHT FLUSH')
+					hand[0]=9;
+				}
+		}
+		if(hearths>4){
+			var highhearts=[];
+			for(var i=0;i<player.length;i++){
+				if(player[i]>25&&player[i]<39){
+					highhearts.push(player[i]-25);
+				}
+			}
+			highhearts.sort(function(a, b){return a-b});
+			console.log("FLUSH");
+			if(highhearts[0]==1){
+				highhearts.push(14);
+			}
+			highhearts.sort(function(a, b){return a-b});
+			if(hand[0]<5){
+				for(var i=0;i<highhearts.length;i++){
+					console.log(highhearts[i]+1)
+					hand.unshift(highhearts[i]+1);
+				}
+				hand.unshift(6);}
+				else if(hand[0]==5){
+					console.log('STRAIGHT FLUSH')
+					hand[0]=9;
+				}
+		}
+	}
 })
